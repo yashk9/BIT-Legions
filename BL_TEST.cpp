ae8711c3f041a2682fc06b8ad4308f14dea18b75
AvDevx <avats765@gmail.com>
2019-08-31 13:03:16 +0530

    Q1Solve

--- BL_TEST.cpp
+++ BL_TEST.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
 using namespace std;
 
+int evenOdd()
+{
+	int a;
+	cout<<"\nEnter a number:";
+	cin>>a;
+	if(a%2 == 0)
+		cout<<"\nThe number is even";
+	else
+		cout<<"\nThe number is odd";
+	
+}
+
 int main()
 {
 	cout<<"Hello legions!";
+
+	/*Program 1*/
+	evenOdd();
 }
