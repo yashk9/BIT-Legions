5cc72738e9970a664b0b207d6581b979f0db00af
Anshuman vats <avats765@gmail.com>
2019-09-27 17:47:39 +0530

    Test ready

--- BL_TEST.cpp
+++ BL_TEST.cpp
@@ -1,8 +1,79 @@
 #include <iostream>
 using namespace std;
 
+/*
+Select a problem from the following list and solve it.
+its okay if you want to solve a question that has already been solved.
+Create a user defined funtion and call it inside the main funtion.
+Happy coding!
+*/
+
+/* Q1 )  Write a program in C++ that converts kilometers per hour to meter per second.
+	 Start your code here (where this comment ends).
+	 for ease comment your name email 
+*/
+
+
+
+/* Q2 )  Write a program in C++ which swap the values of two variables not using third variable
+	 Start your code here (where this comment ends).
+	 for ease comment your name email 
+*/
+
+
+
+/* Q3 )  Write a program in C++ to display distinct elements from an array.
+	 Start your code here (where this comment ends).
+	 for ease comment your name email 
+*/
+
+
+
+/* Q4 ) Write a program in C++ to find the area and circumference of a circle.
+	Start your code here (where this comment ends).
+	for ease comment your name email 
+*/
+
+
+
+
+/* Q5 ) Write a program in C++ to find the sum of the array elements.
+	Start your code here (where this comment ends).
+	for ease comment your name email 
+*/
+
+
+
+/* Q6 ) Write a program in C++ to implement binary search.
+	Start your code here (where this comment ends).
+	for ease comment your name email 
+*/
+
+
+/* Q7 ) Write a program in C++ to find the largest smallest and average of the array.
+	Start your code here (where this comment ends).
+	for ease comment your name email 
+*/
+
+
+/* Q8 ) Write a program in C++ to reverse a string.
+	Start your code here (where this comment ends).
+	for ease comment your name email 
+*/
+
+
+/* Q9 ) Write a program in C++ to check if string is palindrome.
+	Start your code here (where this comment ends).
+	for ease comment your name email 
+*/
+
+
+/* Q10 ) Write a program in C++ to insert a element in linked list.
+	Start your code here (where this comment ends).
+	for ease comment your name email 
+*/
+
 int main()
 {
-	cout<<"Hey this is dipak saini!";
-	cout<<"Hello legions!";
+
 }
